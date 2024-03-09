ee348ac26ce5ca597de50c8e2baa9606981d1798
richard <richardvalles94@gmail.com>
2024-03-09 09:07:02 -0600

    database_conect

--- database.py
+++ database.py
@@ -1,9 +1,25 @@
 import mysql.connector
 import os
 
-def _init_(self, host, user, password, database):
-    self.host = host
-    self.user = user
-    self.pw = password
-    self.db = database
+class MySQLDB:
+    def _init_(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.pw = password
+        self.db = database
 
+def conect(self):
+    try: 
+        if(self.connect == None):
+            self.connection = mysql.connector.connect(
+            host = self.host, 
+            user  = self.user, 
+            paassword = self.pw, 
+            database = self.db
+        )
+
+    except mysql.connector.Error as error:
+        print("ERROR MIENTRAS SE ESTABA CONECTANDO LA BASE DE DATOS")
+
+
+db = MySQLDB("localhost","root","","dateslp")
