afccbaa8d52e78c2ed18ca622f6086aa9782328f
richard <richardvalles94@gmail.com>
2024-03-09 08:45:45 -0600

    chages

--- database.py
+++ database.py
@@ -1,3 +1,8 @@
 import mysql.connector
-import.os 
+import os
 
+def _init_(self, host, user, password, database):
+    self.host = host
+    self.user = user
+    self.pw = password
+    self.db = database
