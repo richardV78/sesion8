a78c326b098f9b05ff2fb45f1297270b322f2ea8
richard <richardvalles94@gmail.com>
2024-03-09 09:31:23 -0600

    database

--- database.py
+++ database.py
@@ -7,19 +7,34 @@ class MySQLDB:
         self.user = user
         self.pw = password
         self.db = database
+        self.connection = None
+    def connect(self):
+        try: 
+            if(self.connect == None):
+                self.connection = mysql.connector.connect(
+                    host = self.host, 
+                    user  = self.user, 
+                    paassword = self.pw, 
+                    database = self.db
+                )
+                os.system("colora2")            
+                print("chatel te conectaste")
+        except mysql.connector.Error as error:
+            print("ERROR MIENTRAS SE ESTABA CONECTANDO LA BASE DE DATOS{}".format(error))
 
-def conect(self):
-    try: 
-        if(self.connect == None):
-            self.connection = mysql.connector.connect(
-            host = self.host, 
-            user  = self.user, 
-            paassword = self.pw, 
-            database = self.db
-        )
-
-    except mysql.connector.Error as error:
-        print("ERROR MIENTRAS SE ESTABA CONECTANDO LA BASE DE DATOS")
+    def disconnected(self):
+            try: 
+                if self.connection:
+                    self.connection.close
+                    self.connection = None
+            except mysql.connector.Error as error:
+                print("ERROR")                
 
 
-db = MySQLDB("localhost","root","","dateslp")
+
+
+db =MySQLDB("localhost" ,"root", "", "test-lp")
+print("conectado")
+
+db.connect()
+db.disconnected()
